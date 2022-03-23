3f4f4c755c392e304e5bf143d75ed49071a5602e
user5559 <89919847+user5559@users.noreply.github.com>
2022-03-23 09:40:56 -0400

    what i did in class

--- Dating sim/d_game.py
+++ Dating sim/d_game.py
@@ -1,5 +1,5 @@
 import pygame, os,ctypes
-os.chdir('c://sidews')
+os.chdir('c://Dating sim')
 from pygame.locals import(
     KEYDOWN,
     QUIT,
@@ -11,12 +11,18 @@ pygame.init()
 
 user32 = ctypes.windll.user32
 screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+screensize = list(screensize)
+SCREEN_WIDTH = screensize[0]
+SCREEN_HEIGHT = screensize[1]
 
 screen = pygame.display.set_mode((screensize)) 
 
 running = True
 
-
+background_color = 255,182,193
+#255,182,193
+Rickimage = pygame.image.load('gorrillaz.png')
+text_box = pygame.image.load('dating sim textbox.png')
 
 while running:
     for event in pygame.event.get():
@@ -25,5 +31,12 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-    screen.fill((255,182,193))
+    
+    screen.fill((background_color))
+
+    screen.blit(Rickimage, (SCREEN_WIDTH/2.5, SCREEN_HEIGHT/4))
+    screen.blit(text_box, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+
     pygame.display.update()
