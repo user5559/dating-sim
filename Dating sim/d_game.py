d8984fe827aa94fb80c66d5d05333908dab715a6
user5559 <89919847+user5559@users.noreply.github.com>
2022-03-23 09:59:43 -0400

    Update d_game.py

--- Dating sim/d_game.py
+++ Dating sim/d_game.py
@@ -19,10 +19,15 @@ screen = pygame.display.set_mode((screensize))
 
 running = True
 
+color = 255,255,255
 background_color = 255,182,193
 #255,182,193
 Rickimage = pygame.image.load('gorrillaz.png')
 text_box = pygame.image.load('dating sim textbox.png')
+
+smallfont = pygame.font.SysFont('Corbel', 35)
+
+text = smallfont.render('Next', True, color)
 
 while running:
     for event in pygame.event.get():
@@ -35,8 +40,6 @@ while running:
     screen.fill((background_color))
 
     screen.blit(Rickimage, (SCREEN_WIDTH/2.5, SCREEN_HEIGHT/4))
-    screen.blit(text_box, (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
+    screen.blit(text_box, (SCREEN_WIDTH/3.4, SCREEN_HEIGHT/1.6))
 
     pygame.display.update()
